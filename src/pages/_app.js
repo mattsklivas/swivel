620ea86adc1f2f9ce99d3a6d087fa997504091c2
mattsklivas <msklivas@live.com>
2022-09-12 11:13:06 -0400

    Test husky

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,8 @@ import React from 'react'
 // Antd-specific imports
 import 'antd/dist/antd.css'
 
+const test = true
+
 function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
