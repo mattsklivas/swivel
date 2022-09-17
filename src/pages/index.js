80196ab7405f0133844060772cb41f290ce5be93
root <root@Jack-Laptop.localdomain>
2022-09-17 16:04:10 -0400

    added another button for testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,13 @@ import React from 'react'
 import { Button, Space, DatePicker } from 'antd'
 
 function Home() {
-  const onChange = () => {}
+  const onChange = () => { }
   return (
     <div style={{ padding: 100 }}>
       <Space direction="vertical">
         <Button type="primary">Primary Button</Button>
         <Button type="ghost">Ghost Button</Button>
+        <Button type="primary">btntest</Button>
         <DatePicker onChange={onChange} />
       </Space>
     </div>
