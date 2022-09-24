4b350c7ff1a2876c6756c04d3d48107c2171017e
Tarun-Elango <tarunelango77@gmail.com>
2022-09-24 18:21:23 -0400

    *reformating

--- src/pages/auth0.js
+++ src/pages/auth0.js
@@ -1,9 +1,9 @@
 import { initAuth0 } from '@auth0/nextjs-auth0'
 
 export default initAuth0({
-  secret: 'process.env.NEXT_PUBLIC_AUTH0_SECRET',
-  issuerBaseURL: 'https://dev-gl5357kx.us.auth0.com',
-  baseURL: 'http://localhost:3000',
-  clientID: 'JRfLd06JxXfnzE2wDOdbO6iJaWmDYeWQ',
-  clientSecret: '0aAMy3OkAo4CAVUicd4xxnVooHZtkKgBMLKB0xHO0Rl7SaH6DvmfHJ-wc01emIcR',
+  secret: 'process.env.NEXT_PUBLIC_AUTH0_SECRETdfasdfadsfasdfasdfavsdvf',
+  issuerBaseURL: process.env.NEXT_PUBLIC_AUTH0_ISSUER_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_AUTH0_BASE_URL,
+  clientID: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID,
+  clientSecret: process.env.NEXT_PUBLIC_AUTH0_CLIENT_SECRET,
 })
